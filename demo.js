411f5506893f4a541caadb68a6e2f42f40cb8fe0
albster <albster@mac.com>
2019-02-08 17:30:46 -0500

    ruff draft

---
+++ demo.js
@@ -0,0 +1,18 @@
+Alert('you have 10 guesses. Guess 1-10');
+
+var answer=3;
+var guess;
+
+for(i=0;1<10;inewFunction()++){
+    guess = prompt("Whats your guess");
+    if(answer == guess){
+               alert("You guessed correctly");
+               break;
+    }else{
+        guess = prompt("Please Try Again");
+    }
+}
+
+function newFunction() {
+    ;
+}
